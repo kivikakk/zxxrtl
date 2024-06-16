b56e2ee4c871aa0ef71abff5b444849a6f4ce519
Asherah Connor <ashe@kivikakk.ee>
2024-06-16 16:37:57 +0300

    build.zig: add guessYosysDataDir.

--- build.zig
+++ build.zig
@@ -30,3 +30,12 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
+
+pub fn guessYosysDataDir(b: *std.Build) []const u8 {
+    const result = std.process.Child.run(.{
+        .allocator = b.allocator,
+        .argv = &.{ "yosys-config", "--datdir" },
+        .expand_arg0 = .expand,
+    }) catch @panic("couldn't run yosys-config; please supply -Dyosys_data_dir");
+    return std.mem.trim(u8, result.stdout, "\n");
+}
