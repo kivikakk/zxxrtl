5c7849b7e37a66da51226a6f3a9ca0c482bb8882
Asherah Connor <ashe@kivikakk.ee>
2024-06-16 16:27:23 +0300

    add a module.

--- build.zig
+++ build.zig
@@ -4,6 +4,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("zxxrtl", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "zxxrtl",
         .root_source_file = b.path("src/root.zig"),
